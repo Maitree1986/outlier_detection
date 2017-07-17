89b8b3072ef7057618d675c9fcde847dfacedb12
Maitree Priyadarsini <priyadarsini.maitree23@gmail.com>
2017-07-17 23:02:16 +0530

    Adding license

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'categorical_outlier',
+    packages = ['categorical_outlier'],
+    version = '001',  # Ideally should be same as your GitHub release tag varsion
+    description = 'This program separates the most uncommon pattern',
+    author = 'Maitree Priyadarsini',
+    author_email = 'priyadarsini.maitree23@gmail.com',
+    url = 'https://github.com/Maitree1986/outlier_detection',
+    download_url = 'https://github.com/Maitree1986/outlier_detection.git',
+    keywords = ['categorical', 'outlier'],
+    classifiers = [],
+)
