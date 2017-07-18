40b7b9277e210f48aa810307e2394e76cd5c77a7
Maitree Priyadarsini <priyadarsini.maitree23@gmail.com>
2017-07-18 13:17:54 +0530

    Update outlie.py

--- outlie.py
+++ outlie.py
@@ -6,8 +6,9 @@ def outlie(data,threshold=1):
         data=data.applymap(str)
 ## This line combines all the column values to one single array to find the pattern combination of all the columns          
         concat = data.apply(lambda x: ''.join(x),axis=1)
-## This line gets a array with the count
+## This line gets a array with the frequency of each pattern in the file
         count=concat.apply(lambda x:list(concat).count(x))
+## This line g
         result=count.apply(lambda x:1 if x>threshold else -1)
         return(result)
         
